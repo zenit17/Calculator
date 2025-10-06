232f08d9b98d400f52090cda50187b8f7dc4896d
Zenit <Zenit17days@gmail.com>
2025-10-06 09:25:53 +0800

    до багов готово

--- script.js
+++ script.js
@@ -1,43 +1,63 @@
-window.onload = function () {
+window.onload = () => {
     let inputfield = document.getElementById('inputfield');
+    let buttonravno = document.getElementById('ravno');
     let butDigits = document.querySelectorAll('.digits-wrap button');
     let butMathOperations = document.querySelectorAll('.math-wrap button')
-    let end = document.getElementById('=')
     let number1 = 0;
     let number2 = 0;
-    let clean = 0;
+    let clean = 0
+    let mathOperation = ''
 
+    //обработка нажатий цифр
     butDigits.forEach(
         (el) => {
             el.addEventListener('click', (e) => {
                 console.log(e.target.innerText)
-                inputfield.value = e.target.innerText + inputfield.value
                 if (clean === 1) {
                     inputfield.value = '';
-                    clean = 0
+                    clean = 0;
+                }
+                inputfield.value = inputfield.value + e.target.innerText
+            })
+        }
+    )
+
+//обработка математических операций
+    butMathOperations.forEach(
+        (el) => {
+            el.addEventListener('click', (e) => {
+                if (e.target.innerText !== '=') {
+                    number1 = inputfield.value
+                    clean = 1;
+                    mathOperation = e.target.innerText
+                    console.log(e.target.innerText)
                 }
             })
         }
     )
 
+    buttonravno.addEventListener('click', (event) => {
+            console.log(number1);
+            console.log(mathOperation);
+            console.log(inputfield.value);
 
-    butMathOperations.forEach(
-        (el) => {
-            el.addEventListener('click', (e) => {
-                number1 = inputfield.value
-                clean = 1;
-                number2 = inputfield.value
-                console.log(e.target.innerText)
-            })
+
+            if (mathOperation === '+') {
+                inputfield.value = +number1 + +inputfield.value
+            }
+            if (mathOperation === '-') {
+                inputfield.value = +number1 - +inputfield.value
+            }
+            if (mathOperation === '*') {
+                inputfield.value = +number1 * +inputfield.value
+            }
+            if (mathOperation === '/') {
+                inputfield.value = +number1 / +inputfield.value
+            }
         }
     )
-}
 
-end.addEventListener('click', (e) => {
-        if (clean === 1) {
-            inputfield.value = '';
-            clean = 0
-        }
-        inputfield.value = number1.value + number2.value
-    }
-)
+}
+//TODO
+// деление на 0
+// первый 0
